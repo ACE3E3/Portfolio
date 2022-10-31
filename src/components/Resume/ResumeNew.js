e563c26af3abc9d7be774a6666c78621c8cc6a7d
ACE3E3 <53176174+ACE3E3@users.noreply.github.com>
2022-10-31 15:08:52 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/ACE3E3/Portfolio/blob/master/src/Assets/zaid_cv.pdf";
+  "https://raw.githubusercontent.com/ACE3E3/Portfolio/master/src/Assets/zaid_cv.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
